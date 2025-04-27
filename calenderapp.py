bab0d92ececc691787c6769b3d0217b6caef0461
sc-00 <sulingchu106@icloud.com>
2025-04-27 12:50:06 +0100

    Add files via upload

--- calenderapp.py
+++ calenderapp.py
@@ -1,22 +1,35 @@
-import tkinter as tk
 from tkinter import *
-from tkinter.ttk import *
-from tkinter import messagebox
-from tkinter.filedialog import *
 import tkinter.font as f
+import calendar
 
 root = Tk()
 root.geometry("350x200")
 root.title("calender")
 root.config(background = "turquoise")
 
-title = Label(root,text = "Calender",font = f.Font(size = 30))
+def showcalendar():
+    newwindow = Tk()
+    newwindow.config(background = "wheat")
+    newwindow.title("Calendar")
+    getyear = int(yearbox.get())
+    calendarcontent = calendar.calendar(getyear)
+    calendarshow = Label(newwindow,text = calendarcontent)
+    calendarshow.grid(row = 5, column = 1,padx = 20)
+    newwindow.mainloop
+
+title = Label(root,text = "Calendar",font = f.Font(size = 30))
 title.place(x = 90,y = 0)
 
 label1 = Label(root,text = "enter the year",font = ("Arial",15))
-label1.place(x = 105, y = 55)
+label1.place(x = 105, y = 50)
 
 yearbox = Entry(root)
 yearbox.place(x = 30, y = 80,width = 270)
+
+calendarappear = Button(root,text = "Show Calendar",command = showcalendar,font = ("Arial",10))
+calendarappear.place(x = 120, y = 110)
+
+exitb = Button(root, text = "Exit",font = ("Arial",10),command = root.destroy)
+exitb.place(x = 125,y = 140)
 
 root.mainloop()
